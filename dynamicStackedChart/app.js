694f22ae90db5ba4a33c22c78e75dac8082efc90
Yizhe Chen <yzchen1991@gmail.com>
2015-11-16 15:11:36 -0800

    add dynamic stacked chart component

---
+++ dynamicStackedChart/app.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var COMMENTS_FILE = path.join(__dirname, 'data.json');
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/data', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
