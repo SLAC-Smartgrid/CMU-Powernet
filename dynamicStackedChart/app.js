951110461959abfe3126b614067b854439032de1
Cory <corypruce@gmail.com>
2015-11-22 18:38:04 -0800

    mongo works

--- dynamicStackedChart/app.js
+++ dynamicStackedChart/app.js
@@ -6,7 +6,8 @@ var app = express();
 
 var COMMENTS_FILE = path.join(__dirname, 'data.json');
 
-var mongo = require('mongodb');
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -23,6 +24,34 @@ app.get('/api/data', function(req, res) {
     res.setHeader('Cache-Control', 'no-cache');
     res.json(JSON.parse(data));
   });
+});
+
+
+app.get('/hh', function(req, res) {
+    
+    // Connection URL
+    var url = 'mongodb://localhost:27017/PowernetDB';
+    // Use connect method to connect to the Server
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        
+        var collection = db.collection('homehubs');
+        // Find some documents
+        collection.find({}).toArray(function(err, docs) {
+            assert.equal(err, null);
+            //assert.equal(2, docs.length);
+            console.log("Found the following records");
+            console.dir(docs);
+            
+            
+            res.json(docs);
+            db.close(); 
+        });
+       
+    }); 
+    
+ 
 });
 
 app.listen(app.get('port'), function() {
