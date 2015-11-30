5de2ed79a69f9d2f4195db5fc79038ae036e27f6
Yizhe Chen <yzchen1991@gmail.com>
2015-11-29 23:38:12 -0800

    move bootstrap admin panel old stain css and js dependency into bootstrap index.js file so it'll be generated automatically.

--- bootstrap_dashboard/public/js/bootstrap/index.js
+++ bootstrap_dashboard/public/js/bootstrap/index.js
@@ -2,3 +2,7 @@ import NavSidebar from './navigation.jsx';
 import HHStatusPanel from './HHStatusPanel.jsx';
 import PowerConsumptionMonitorPanel from './powerConsumptionMonitorPanel.jsx';
 import PowerConsumptionPieCompositionPanel from './powerConsumptionPieChart.jsx';
+import './sb-admin-2.js';
+
+import '../../css/timeline.css';
+import '../../css/sb-admin-2.css';
