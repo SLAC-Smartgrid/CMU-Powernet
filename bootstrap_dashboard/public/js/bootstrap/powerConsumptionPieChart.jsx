48c52b15e9f21ab9e3f586eec96fe270f49483ab
Yizhe Chen <yzchen1991@gmail.com>
2015-11-29 22:59:25 -0800

    add pie chart for demonstration on dashboard

---
+++ bootstrap_dashboard/public/js/bootstrap/powerConsumptionPieChart.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DataBox from '../nvdcharts/stackedAreaChart.jsx';
+import DonutPieChartBox from '../nvdcharts/pieChart.jsx';
+import '../../css/dashboard.css';
+
+var PowerConsumptionPieCompositionPanel = React.createClass({
+
+    render: function() {
+        return (
+          <div className="col-lg-4">
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                  <i className="fa fa-bar-chart-o fa-fw"></i> Power Consumption Composition
+                </div>
+                <div className="panel-body" id="powerConsumptionPieChart" >
+                    < DonutPieChartBox />
+                </div>
+            </div>
+          </div>
+        );
+    }
+})
+
+
+ReactDOM.render(
+  < PowerConsumptionPieCompositionPanel />,
+  document.getElementById('react-PowerConsumptionPieChart')
+);
+
+
