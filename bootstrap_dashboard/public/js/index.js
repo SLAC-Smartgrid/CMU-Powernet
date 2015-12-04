6c6587c42689c0a89aa0590e77c28222e36d096d
pravkreddy <pravk.reddy@gmail.com>
2015-12-04 00:32:03 -0800

    apply patch

--- bootstrap_dashboard/public/js/index.js
+++ bootstrap_dashboard/public/js/index.js
@@ -3,3 +3,4 @@ import HHStatusPanel from './components/bootstrap/HHStatusPanel.jsx'
 import HHStatusTable from './components/bootstrap/HHStatusTable.jsx';
 import PowerConsumptionMonitorPanel from './components/bootstrap/powerConsumptionMonitorPanel.jsx';
 import PowerConsumptionPieCompositionPanel from './components/bootstrap/powerConsumptionPieChart.jsx';
+import PowerConsumptionBarCompositionPanel from './components/bootstrap/powerConsumptionBarChart.jsx';
